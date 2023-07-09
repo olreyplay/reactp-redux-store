b73d1cc55dad5bfde9d7fa8882e7b2132e55bfff
Oleg <126164012+olreyplay@users.noreply.github.com>
2023-07-10 01:26:53 +0300

    v2

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import shipReducer from "./reducers/shipReducer";
+
+const store = configureStore({
+  reducer: {
+    ships: shipReducer,
+  },
+});
+
+export default store;
